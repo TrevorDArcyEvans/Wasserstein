8d703792a647baeb4cd3bfa337781bafab7e906c
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2026-07-03 09:48:26 +0100

    - refactored into separate assy

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace Wasserstein;
 
-using System.Numerics;
+using Wasserstein.API;
 
 public static class Program
 {
@@ -10,67 +10,10 @@ public static class Program
     var Q1 = new[] {0.1, 0.1, 0.6, 0.1, 0.1};
     var Q2 = new[] {0.1, 0.1, 0.1, 0.1, 0.6};
 
-    var wass_p_q1 = Wasserstein(P, Q1);
-    var wass_p_q2 = Wasserstein(P, Q2);
+    var wass_p_q1 = WassersteinAPI.Wasserstein(P, Q1);
+    var wass_p_q2 = WassersteinAPI.Wasserstein(P, Q2);
 
     Console.WriteLine($"Wasserstein(P, Q1) = {wass_p_q1:F4}");
     Console.WriteLine($"Wasserstein(P, Q2) = {wass_p_q2:F4}");
-  }
-
-  private static int FirstNonZero<T>(T[] vec) where T:INumber<T>
-  {
-    var dim = vec.Length;
-    for (var i = 0; i < dim; ++i)
-    {
-      if (vec[i] > T.Zero)
-      {
-        return i;
-      }
-    }
-
-    return -1;
-  }
-
-  private static T MoveDirt<T>(T[] dirt, int dirtIdx, T[] holes, int holeIdx) where  T : INumber<T>
-  {
-    var flow = T.Zero;
-    var dist = 0;
-    if (dirt[dirtIdx] <= holes[holeIdx])
-    {
-      flow = dirt[dirtIdx];
-      dirt[dirtIdx] = T.Zero;
-      holes[holeIdx] -= flow;
-    }
-    else if (dirt[dirtIdx] > holes[holeIdx])
-    {
-      flow = holes[holeIdx];
-      dirt[dirtIdx] -= flow;
-      holes[holeIdx] = T.Zero;
-    }
-
-    dist = Math.Abs(dirtIdx - holeIdx);
-
-    return flow * (T)Convert.ChangeType(dist, typeof(T));
-  }
-
-  private static T Wasserstein<T>(T[] p, T[] q) where  T : INumber<T>
-  {
-    var dirt = (T[]) p.Clone();
-    var holes = (T[]) q.Clone();
-    var totalWork = T.Zero;
-    while (true)
-    {
-      var fromIdx = FirstNonZero(dirt);
-      var toIdx = FirstNonZero(holes);
-      if (fromIdx == -1 || toIdx == -1)
-      {
-        break;
-      }
-
-      var work = MoveDirt(dirt, fromIdx, holes, toIdx);
-      totalWork += work;
-    }
-
-    return totalWork;
   }
 }
---
+++ Wasserstein.API/WassersteinAPI.cs
@@ -0,0 +1,63 @@
+﻿namespace Wasserstein.API;
+
+using System.Numerics;
+
+public static class WassersteinAPI
+{
+  private static int FirstNonZero<T>(T[] vec) where T:INumber<T>
+  {
+    var dim = vec.Length;
+    for (var i = 0; i < dim; ++i)
+    {
+      if (vec[i] > T.Zero)
+      {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  private static T MoveDirt<T>(T[] dirt, int dirtIdx, T[] holes, int holeIdx) where  T : INumber<T>
+  {
+    var flow = T.Zero;
+    var dist = 0;
+    if (dirt[dirtIdx] <= holes[holeIdx])
+    {
+      flow = dirt[dirtIdx];
+      dirt[dirtIdx] = T.Zero;
+      holes[holeIdx] -= flow;
+    }
+    else if (dirt[dirtIdx] > holes[holeIdx])
+    {
+      flow = holes[holeIdx];
+      dirt[dirtIdx] -= flow;
+      holes[holeIdx] = T.Zero;
+    }
+
+    dist = Math.Abs(dirtIdx - holeIdx);
+
+    return flow * (T)Convert.ChangeType(dist, typeof(T));
+  }
+
+  public static T Wasserstein<T>(T[] p, T[] q) where  T : INumber<T>
+  {
+    var dirt = (T[]) p.Clone();
+    var holes = (T[]) q.Clone();
+    var totalWork = T.Zero;
+    while (true)
+    {
+      var fromIdx = FirstNonZero(dirt);
+      var toIdx = FirstNonZero(holes);
+      if (fromIdx == -1 || toIdx == -1)
+      {
+        break;
+      }
+
+      var work = MoveDirt(dirt, fromIdx, holes, toIdx);
+      totalWork += work;
+    }
+
+    return totalWork;
+  }
+}
